82e7f5b1316961b2d2c5166c0b9501b4f25d7cd9
jchan3990 <jonchan3990@gmail.com>
2020-06-24 00:10:20 -0700

    Added second test

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from '../client/src/components/App.jsx';
 import {shallow, mount, render} from 'enzyme';
-import Promise from 'bluebird';
 
 describe('<App /> rendering', () => {
   let wrapper;
@@ -15,5 +14,10 @@ describe('<App /> rendering', () => {
     instance.componentDidMount();
     expect(instance.getPhotos).toHaveBeenCalledTimes(1);
   });
+
+  it(`should check pictures to not be empty`, () => {
+    const instance = wrapper.instance();
+    expect(instance.state.homeActive).toEqual(true);
+  })
 
 });
