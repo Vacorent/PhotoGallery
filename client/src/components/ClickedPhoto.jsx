1bfee9ea9397dd41b9aed1372ef6204523b0c252
jchan3990 <jonchan3990@gmail.com>
2020-06-26 01:40:43 -0700

    Added close button on individual photo page

--- client/src/components/ClickedPhoto.jsx
+++ client/src/components/ClickedPhoto.jsx
@@ -7,12 +7,15 @@ class ClickedPhoto extends React.Component {
 
   render() {
     return (
-    <div className="clickedPhoto">
-      <div>
-        <img className="currentPhoto" src={this.props.photo.src}/>
+    <div className="photo-container">
+      <div className="photo-wrapper">
+        <img className="current-photo" src={this.props.photo.src}/>
+      </div>
+      <div className="description-wrapper">
+        <span className="current-description">{this.props.photo.alt}</span>
       </div>
       <div>
-        <span className="currentDescription">{this.props.photo.alt}</span>
+        <button type="button" class="close-btn-container"><span class="close-btn-wrapper"><span class="close-btn-x"><svg viewBox="0 0 12 12" role="presentation" aria-hidden="true" focusable="false"><path d="m11.5 10.5c.3.3.3.8 0 1.1s-.8.3-1.1 0l-4.4-4.5-4.5 4.5c-.3.3-.8.3-1.1 0s-.3-.8 0-1.1l4.5-4.5-4.4-4.5c-.3-.3-.3-.8 0-1.1s.8-.3 1.1 0l4.4 4.5 4.5-4.5c.3-.3.8-.3 1.1 0s .3.8 0 1.1l-4.5 4.5z" fill-rule="evenodd"></path></svg></span><span class="close-btn-text">Close</span></span></button>
       </div>
     </div>
     )
