fb3a1bf1394c4160303e395a62be822675448eb2
jchan3990 <jonchan3990@gmail.com>
2020-06-30 18:16:00 -0700

    remove console logs

--- server/controller.js
+++ server/controller.js
@@ -5,10 +5,8 @@ module.exports = {
   getPhotos: (req, res) => {
     model.getPhotos((err, data) => {
       if (err) {
-        console.log('Server get photos FAIL');
         res.status(400).send(err);
       } else {
-        console.log('Server get photos SUCCESS');
         res.status(201).send(data);
       }
     });
@@ -16,10 +14,8 @@ module.exports = {
   getReviews: (req, res) => {
     model.getReviews((err, data) => {
       if (err) {
-        console.log('Server get reviews FAIL');
         res.status(400).send(err);
       } else {
-        console.log('Server get reviews SUCCESS');
         res.status(201).send(data);
       }
     });
